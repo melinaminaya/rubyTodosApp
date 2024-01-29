dfdffa8572354e9a91752762c766909146305c24
Melina Minaya <melina.minaya@autotrac.com.br>
2024-01-29 15:27:10 -0300

    Insert views and styles for login up, login in
    Insert relations between user and posts and other links stylization.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_26_183644) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_181610) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_26_183644) do
     t.string "keywords"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
